b1e3d34e78798faf1d657c964cae549611aa43db
Merlin Förster <merlin.foerster@live.de>
2020-05-23 23:34:21 +0200

    removed reload

--- script.js
+++ script.js
@@ -4,22 +4,30 @@ ul = document.querySelector("ul");
 instuctionsText = document.querySelector("p");
 alcoholicText = document.getElementsByClassName("alcoholic")[0];
 glassText = document.getElementsByClassName("glass")[0];
+button = document.querySelector("button");
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-  .then((res) => res.json())
-  .then((data) => {
-    drinkNameText.textContent = data.drinks[0].strDrink;
-    cocktailImage.src = data.drinks[0].strDrinkThumb;
-    instuctionsText.textContent = data.drinks[0].strInstructions;
-    alcoholicText.textContent = data.drinks[0].strAlcoholic.toLowerCase();
-    glassText.textContent = data.drinks[0].strGlass.toLowerCase();
+button.addEventListener("click", getCocktail, false);
 
-    for (let i = 21; i < 36; i++) {
-      const liElement = document.createElement("li");
-      Object.values(data.drinks[0])[i];
-      liElement.innerText =
-        Object.values(data.drinks[0])[i + 15].toLowerCase() +
-        Object.values(data.drinks[0])[i].toLowerCase();
-      ul.appendChild(liElement);
-    }
-  }); //usually I would add a catch here, that I don't do it here has a reason ;)
+getCocktail();
+
+function getCocktail() {
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    .then((res) => res.json())
+    .then((data) => {
+      drinkNameText.textContent = data.drinks[0].strDrink;
+      cocktailImage.src = data.drinks[0].strDrinkThumb;
+      instuctionsText.textContent = data.drinks[0].strInstructions;
+      alcoholicText.textContent = data.drinks[0].strAlcoholic.toLowerCase();
+      glassText.textContent = data.drinks[0].strGlass.toLowerCase();
+
+      for (let i = 21; i < 36; i++) {
+        const liElement = document.createElement("li");
+        Object.values(data.drinks[0])[i];
+        liElement.innerText =
+          Object.values(data.drinks[0])[i + 15].toLowerCase() +
+          Object.values(data.drinks[0])[i].toLowerCase();
+        ul.appendChild(liElement);
+      }
+    })
+    .catch((error) => console.log(/*error*/));
+}
